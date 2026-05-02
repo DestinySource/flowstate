77353cd42e1aa783a8c4e82505d1bdb0b19124c3
Yodieyogaming <groendyl@gmail.com>
2026-05-02 22:52:59 +0200

    Add Base of the project

---
+++ src/main/java/io/DestinySource/Flowstate/FlowstateApplication.java
@@ -0,0 +1,14 @@
+package io.DestinySource.Flowstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FlowstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowstateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/DestinySource/Flowstate/FlowstateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.DestinySource.Flowstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
