7442990fd48592d681868f6a7f0a3c9c77919a63
Dylan <dylan.kuiper@hva.nl>
2026-05-07 16:08:52 +0200

    Fix: alter the config and add new imports

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import {fileURLToPath, URL} from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    basicSsl()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    https: true,
+    port: 5173
+  }
 })
