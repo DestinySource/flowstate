2e90e1b540538c1a25ec61d66e3469b03b074659
Dylan <dylan.kuiper@hva.nl>
2026-05-29 17:19:53 +0200

    Feat: add jwt and route securing

---
+++ src/main/java/io/DestinySource/Flowstate/service/JwtService.java
@@ -0,0 +1,62 @@
+    package io.DestinySource.Flowstate.service;
+
+    import io.DestinySource.Flowstate.model.User;
+    import io.jsonwebtoken.Claims;
+    import io.jsonwebtoken.Jwts;
+    import io.jsonwebtoken.security.Keys;
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.security.authentication.BadCredentialsException;
+    import org.springframework.stereotype.Service;
+
+    import javax.crypto.SecretKey;
+    import java.nio.charset.StandardCharsets;
+    import java.util.Date;
+
+    @Service
+    public class JwtService {
+
+        @Value("${jwt.secretKey}")
+        private String jwtSecretKey;
+
+        private SecretKey getSecretKey() {
+            return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+        }
+
+        public String generateAccessToken(User user){
+            return Jwts.builder()
+                    .subject(user.getId().toString())
+                    .claim("type", "access")
+                    .claim("username", user.getUsername())
+                    .claim("role", user.getRole().name())
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                    .signWith(getSecretKey())
+                    .compact();
+        }
+
+        public String generateRefreshToken(User user){
+            return Jwts.builder()
+                    .subject(user.getId().toString())
+                    .claim("type", "refresh")
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                    .signWith(getSecretKey())
+                    .compact();
+        }
+
+        public Long getUserIdFromToken(String token, String expectedType) {
+            Claims claims = Jwts.parser()
+                    .verifyWith(getSecretKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            String type = claims.get("type", String.class);
+
+            if (type == null || !type.equals(expectedType)) {
+                throw new BadCredentialsException("Invalid token type: Expected a " + expectedType + " token.");
+            }
+
+            return Long.valueOf(claims.getSubject());
+        }
+    }
