a06911bc6f27808546ef8e20b60a3500787f1043
Dylan <dylan.kuiper@hva.nl>
2026-06-18 23:38:47 +0200

    refactor: alter to allow all "api/" endpoints

--- src/main/java/io/DestinySource/Flowstate/config/SecurityConfig.java
+++ src/main/java/io/DestinySource/Flowstate/config/SecurityConfig.java
@@ -50,8 +50,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/v1/analytics").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
